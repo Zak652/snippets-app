62b5120d6ab2199b86f9a405ce1a332f206ac8cf
Zak652 <m2kzak@gmail.com>
2017-09-25 23:25:00 +0300

    added the main function to the app

--- snippets.py
+++ snippets.py
@@ -1,4 +1,5 @@
 import logging
+import argparse
 
 # Set the log output file, and the log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
@@ -23,3 +24,36 @@ def get (name):
 	"""
 	logging.error("FIXME: Unimplemented - get({!r})".format(name))
 	return ""
+
+def modify (name, snippet):
+	"""
+	Search for the snippet associated with the name
+
+	Make changes to or replace the snippet
+
+	Show changes made vs original snippet
+
+	Return the snippet
+	"""
+	logging.error("FIXME: Unimplemented - modify({!r}, {!r})".format(name, snippet))
+	return name, snippet
+
+def remove (name):
+	"""
+	Search for the name of the snippet
+
+	Delete the name and the associated snippet
+
+	Return confirmation message
+	"""
+	logging.error("FIXME: Unimplemented - remove({!r})".format(name))
+	return ""
+
+def main():
+	"""Main function"""
+	logging.info("Constructing parser")
+	parser = argparse.ArgumentParser(description = "Store and retrive snippets of text")
+	argument = parser.parse_args()
+
+if __name__=="__main__":
+	main()
